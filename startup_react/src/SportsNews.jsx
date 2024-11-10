2b54e185275dd868f9f0ba95855cc590d22c70a9
cal-jamo <calvin.jameson02@gmail.com>
2024-11-09 23:08:29 -0700

    other changes

---
+++ startup_react/src/SportsNews.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const SportsNews = () => {
+  return (
+    <div>
+      <main className="container bg-secondary text-center my-5 p-4 rounded">
+        <section>
+        <h2>3rd Party API Usage</h2>
+        <p>Placeholder for where current sports news will be displayed using an API.</p>
+        </section>
+      </main>
+    </div>
+  );
+};
+
+export default SportsNews;
