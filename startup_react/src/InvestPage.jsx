74ea310e69135c217fc583506ba4d33b1d02523b
cal-jamo <calvin.jameson02@gmail.com>
2024-11-09 23:07:50 -0700

    Invest and Home page

---
+++ startup_react/src/InvestPage.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const InvestPage = () => {
+  return (
+    <div className="container bg-secondary text-center my-5 rounded">
+      <h2>Invest Page</h2>
+      <p>This is a placeholder for where you will invest in sports teams</p>
+      <img src="https://www.kreedon.in/wp-content/uploads/2023/04/Stock-Market-and-Sports-Betting.jpg" height="300" width="300" title="Stock Market and Sports" alt="Stock Market and Sports" />
+      <h3>Database Data</h3>
+      <p>Placeholder for the teams and their info that will be fetched from MongoDB</p>
+    </div>
+  );
+};
+
+export default InvestPage;
