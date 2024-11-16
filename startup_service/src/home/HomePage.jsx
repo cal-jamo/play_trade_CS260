87816bc319d402dd888ea9b1eb614fa918b6139a
cal-jamo <calvin.jameson02@gmail.com>
2024-11-16 12:07:17 -0700

    UI changes for home page.

--- startup_service/src/home/HomePage.jsx
+++ startup_service/src/home/HomePage.jsx
@@ -7,8 +7,6 @@ const HomePage = () => {
       <p>
         Have you ever wanted to get into investing, but haven't known what to invest in? Maybe you don't understand real estate? Maybe you are nervous to invest because you don't understand why stock prices of companies rise and fall. But what if you could invest in something that you're passionate about? Well now you can with "Play Trade"! Play Trade is an investment platform for all things sports and athletics. Invest in your favorite teams or players. Maybe you are watching someone in college and have a good feeling about the future of their professional career, so you invest in them early on, and WIN BIG when they do! Someone you invested in signs a new endorsement deal? Watch the values of your stock rise. Invest in Play Trade today!
       </p>
-      <h3>WebSocket Data</h3>
-      <p>Placeholder for Real-time data on what other users have invested in</p>
     </div>
   );
 };
