5e548f16f61e44c6a27387413fe07e31f2d9ae3e
cal-jamo <calvin.jameson02@gmail.com>
2024-11-23 23:11:13 -0700

    Changes to connect to mongo db and authenticate users

--- startup_service/service/index.js
+++ startup_service/service/index.js
@@ -1,14 +1,21 @@
-const express = require('express');
+const { MongoClient } = require('mongodb');
 const uuid = require('uuid');
+const bcrypt = require('bcrypt');
+const cookieParser = require('cookie-parser');
+const express = require('express');
+const path = require('path');
+
 const app = express();
 
-// The scores and users are saved in memory and disappear whenever the service is restarted.
-let users = {};
-let scores = [];
+const uri = "mongodb+srv://calvinjameson02:FishBrainsAreGood2@play-trade-260.erubr.mongodb.net/?retryWrites=true&w=majority&appName=play-trade-260";
+const client = new MongoClient(uri);
+const collection = client.db('playtrade_cs260').collection('user');
 
-// The service port. In production the front-end code is statically hosted by the service on the same port.
+app.use(cookieParser());
+
 const port = process.argv.length > 2 ? process.argv[2] : 3000;
 
+// Serve static files
 // JSON body parsing using built-in middleware
 app.use(express.json());
 
@@ -19,44 +26,77 @@ app.use(express.static('public'));
 var apiRouter = express.Router();
 app.use(`/api`, apiRouter);
 
-// CreateAuth a new user
-apiRouter.post('/auth/create', async (req, res) => {
-  const user = users[req.body.email];
-  if (user) {
+// Create user
+app.post('/api/auth/create', async (req, res) => {
+  console.log('Received request to /api/auth/create');
+  if (await getUser(req.body.email)) {
     res.status(409).send({ msg: 'Existing user' });
   } else {
-    const user = { email: req.body.email, password: req.body.password, token: uuid.v4() };
-    users[user.email] = user;
-
-    res.send({ token: user.token });
+    const user = await createUser(req.body.email, req.body.password);
+    setAuthCookie(res, user.token);
+    res.send({ id: user._id });
   }
 });
 
-// GetAuth login an existing user
-apiRouter.post('/auth/login', async (req, res) => {
-  const user = users[req.body.email];
+// Login user
+app.post('/api/auth/login', async (req, res) => {
+  console.log('Received request to /api/auth/login');
+  const user = await getUser(req.body.email);
+  if (user && (await bcrypt.compare(req.body.password, user.password))) {
+    setAuthCookie(res, user.token);
+    res.send({ id: user._id });
+  } else {
+    res.status(401).send({ msg: 'Invalid credentials' });
+  }
+});
+
+// Get current user
+app.get('/api/user/me', async (req, res) => {
+  const authToken = req.cookies['token'];
+  const user = await collection.findOne({ token: authToken });
   if (user) {
-    if (req.body.password === user.password) {
-      user.token = uuid.v4();
-      res.send({ token: user.token });
-      return;
-    }
+    res.send({ email: user.email });
+  } else {
+    res.status(401).send({ msg: 'Unauthorized' });
   }
-  res.status(401).send({ msg: 'Unauthorized' });
 });
 
-// DeleteAuth logout a user
-apiRouter.delete('/auth/logout', (req, res) => {
-  const user = Object.values(users).find((u) => u.token === req.body.token);
-  if (user) {
-    delete user.token;
-  }
-  res.status(204).end();
+// Logout user
+app.delete('/api/auth/logout', async (req, res) => {
+  const authToken = req.cookies['token'];
+  await collection.updateOne(
+    { token: authToken },
+    { $set: { token: uuid.v4() } }
+  );
+  res.send({});
 });
 
-// Return the application's default page if the path is unknown
+function getUser(email) {
+  return collection.findOne({ email });
+}
+
+async function createUser(email, password) {
+  const passwordHash = await bcrypt.hash(password, 10);
+  const user = {
+    email,
+    password: passwordHash,
+    token: uuid.v4(),
+  };
+  await collection.insertOne(user);
+  return user;
+}
+
+function setAuthCookie(res, authToken) {
+  res.cookie('token', authToken, {
+    secure: true,
+    httpOnly: true,
+    sameSite: 'strict',
+  });
+}
+
+// Serve React app for all unknown routes
 app.use((_req, res) => {
-  res.sendFile('index.html', { root: 'public' });
+  res.sendFile(path.join(__dirname, '../index.html'));
 });
 
 app.listen(port, () => {
