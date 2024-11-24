53133ef37803c6afe5c831b71416aba710ba351a
cal-jamo <calvin.jameson02@gmail.com>
2024-11-23 23:11:40 -0700

    Changes to the logout function to now use mongo db

--- startup_service/src/login/Authenticated.jsx
+++ startup_service/src/login/Authenticated.jsx
@@ -1,33 +1,34 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
-
 import Button from 'react-bootstrap/Button';
+import { useNavigate } from 'react-router-dom';
 
 export function Authenticated(props) {
     const navigate = useNavigate();
 
-    function logout() {
-        fetch(`/api/auth/logout`, {
-        method: 'delete',
-        })
-        .catch(() => {
-            // Logout failed. Assuming offline
-        })
-        .finally(() => {
+    async function logout() {
+        const response = await fetch('/api/auth/logout', {
+            method: 'delete',
+            headers: {
+                'Content-type': 'application/json; charset=UTF-8',
+            },
+        });
+        if (response?.status === 200) {
             localStorage.removeItem('userName');
             props.onLogout();
-        });
+        } else {
+            console.error('Logout failed');
+        }
     }
 
     return (
         <div>
-        <div className='text-dark mb-2'>{props.userName}</div>
-        <Button variant='primary' onClick={() => navigate('/invest')}>
-            Invest
-        </Button>
-        <Button variant='secondary' onClick={() => logout()}>
-            Logout
-        </Button>
+            <div className='text-dark mb-2'>{props.userName}</div>
+            <Button variant='primary' onClick={() => navigate('/invest')}>
+                Invest
+            </Button>
+            <Button variant='secondary' onClick={() => logout()}>
+                Logout
+            </Button>
         </div>
     );
 }
